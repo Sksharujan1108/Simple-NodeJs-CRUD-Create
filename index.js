2859ad50d96b9d9c6a05a5b689d59660a4f59ec7
@sarujan1 <sarujan@codelantic.com>
2024-06-28 13:43:58 +0530

    update

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello hi');
+const express = require('express')
+const app = express()
+
+// import the port 
+app.listen(3000, () => {
+    console.log('Server Is Working Port 3000');
+});
+
+// Write the get method
+app.get('/', (request, response) => {
+    response.send('Hello World')
+});
